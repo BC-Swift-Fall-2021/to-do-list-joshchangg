289c0c86295ab0ee90115d12d02504e9d953d705
Joshua Chang <joshchangg01@gmail.com>
2021-09-27 10:41:27 -0400

    Ch 5.4 Passing Data Between View Controllers

--- ToDo List/ToDoDetailTableViewController.swift
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 class ToDoDetailTableViewController: UITableViewController {
 
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
-    @IBOutlet weak var nameField: UIView!
+    @IBOutlet weak var nameField: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var noteView: UITextView!
     
+    var toDoItem: String!
+    
     override func viewDidLoad() {
-       super.viewDidLoad()
-
+        super.viewDidLoad()
+        
+        if toDoItem == nil {
+            toDoItem = ""
+        }
+        nameField.text = toDoItem
         
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        toDoItem = nameField.text
+    }
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
         let isPresentingInAddMode = presentingViewController is UINavigationController
